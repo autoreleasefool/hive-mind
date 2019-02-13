4db55c3e691504caef00420954a3ceadc654b922
Joseph Roque <contact@josephroque.ca>
2019-02-12 23:45:29 -0500

    Replace Android things client with iOS

---
+++ client/HiveMind/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HiveMind
+//
+//  Created by Joseph Roque on 2019-02-12.
+//  Copyright © 2019 Joseph Roque. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
--- client/app/src/main/java/ca/josephroque/hivemind/common/ViewAction.kt
+++
@@ -1,7 +0,0 @@
-package ca.josephroque.hivemind.common
-
-/**
- * Copyright (C) 2019 Joseph Roque
- */
-
-interface ViewAction
