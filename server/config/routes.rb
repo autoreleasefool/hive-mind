b4f14243fbdce3f01f161682c14368e67dab04ea
Joseph Roque <contact@josephroque.ca>
2019-03-02 10:42:03 -0500

    Remove a bunch of unused stuff

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  resources :training_images
 
   post 'hivemind/new', to: 'hive_mind#new'
   post 'hivemind/play', to: 'hive_mind#play'
-  post 'hivemind/moves', to: 'hive_mind#available_moves'
 
 end
