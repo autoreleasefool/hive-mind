4682268beafbbb4cb7b609e0d269ddb4216706ad
Joseph Roque <contact@josephroque.ca>
2019-03-16 09:32:41 -0400

    Script for testing hive engine perf without upload

---
+++ script/perftest.sh
@@ -0,0 +1,13 @@
+# Remove cached repo and switch to local
+rm -rf ./build && rm -rf Package.resolved
+sed -i '' 's/git@github\.com:josephroquedev\/hive-engine.git/..\/hive-engine/g' ./Package.swift
+sed -i '' 's/\.\.\/hive-engine/git@github.com:josephroquedev\/hive-engine.git/g' ./Package.swift
+
+# Build and run performance tests
+swift build
+swift test --filter HiveMindCoreTests.CorePerformanceTests/testEvaluationPerformance
+
+# # Reset Package.swift
+rm -rf ./build && rm -rf Package.resolved
+sed -i '' 's/\.\.\/hive-engine/git@github.com:josephroquedev\/hive-engine.git/g' ./Package.swift
+swift build
